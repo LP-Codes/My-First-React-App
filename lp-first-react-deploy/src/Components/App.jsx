c4d2a36a219a25dccb4b8654acbf6e2fe110e8c3
LP-Codes <lovelesh_p@yahoomail.com>
2021-03-24 17:40:30 +0530

    navbar mof

--- lp-first-react-deploy/src/Components/App.jsx
+++ lp-first-react-deploy/src/Components/App.jsx
@@ -8,29 +8,29 @@ import Home from "./Home";
 const App = () => {
   return (
     <div className="container-fluid">
-      <nav class="navbar navbar-expand-sm bg-dark navbar-dark mb-2">
+      <nav class="navbar  navbar-expand bg-dark navbar-dark mb-2">
         <a class="navbar-brand">
           <img src={logo192} alt="logo" style={{ width: "40px" }} />
         </a>
 
         <ul class="navbar-nav">
           <li
-            class="nav-item  font-weight-bolder ml-3  "
+            class="nav-item  font-weight-bolder ml-3 "
             style={{ fontSize: "25px" }}
           >
-            <Link to={"/"}>Home</Link>
+            <Link to={"/"}><i class="fas fa-home fa-2x"></i></Link>
           </li>
           <li
-            class="nav-item mr-2 font-weight-bolder ml-5 "
+            class="nav-item mr-2 font-weight-bolder ml-3 "
             style={{ fontSize: "25px" }}
           >
-            <Link to={"/randomquotes"}>Random Quotes</Link>
+            <Link to={"/randomquotes"}><i class="fab fa-quora fa-2x"></i></Link>
           </li>
           <li
-            class="nav-item mr-2 font-weight-bolder ml-5"
+            class="nav-item mr-2 font-weight-bolder ml-3"
             style={{ fontSize: "25px" }}
           >
-            <Link to={"/issuetracker"}>Issue Tracker</Link>
+            <Link to={"/issuetracker"}><i class="fas fa-bug fa-2x"></i></Link>
           </li>
         </ul>
         <div class="navbar-collapse">
