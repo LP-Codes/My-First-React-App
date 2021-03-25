7e7273a857b2c461cd40128c4fb7d93f66b80f16
LP-Codes <lovelesh_p@yahoomail.com>
2021-03-25 11:51:09 +0530

    fixed css

--- lp-first-react-deploy/src/Components/Youtube.jsx
+++ lp-first-react-deploy/src/Components/Youtube.jsx
@@ -1,69 +1,70 @@
 import React from "react";
+import "./App.css"
 
 const Youtube = () => {
   return (
-    <div>
+    <div style={{backgroundColor:"hsl(0, 0%, 70%)"}}>
       <hr></hr>
       <h2 className="text-center bg-info text-white ">
         Relaxing Music to rejuvenate your mind and body
       </h2>
       <hr></hr>
-      <div className="row">
+      <div className="row container-fluid">
         {" "}
-        <div className="  col -4">
+        <div className="  col ">
           <iframe
-            width="425"
-            height="315"
+          height="300"
+          width="350"
             src="https://www.youtube.com/embed/StVO8lHfzkU"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           ></iframe>
         </div>
-        <div className=" col-4">
+        <div className=" col">
           <iframe
-            width="425"
-            height="315"
+           height="300"
+            width="350"
             src="https://www.youtube.com/embed/kRf_JiuG_-E"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           ></iframe>
         </div>
-        <div className=" col-4">
+        <div className=" col">
           <iframe
-            width="425"
-            height="315"
+            height="300"
+            width="350"
             src="https://www.youtube.com/embed/8uWXx9SO6fk"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           ></iframe>
         </div>
-        <div className=" col-4">
+        <div className=" col">
           <iframe
-            width="425"
-            height="315"
+            height="300"
+            width="350"
             src="https://www.youtube.com/embed/K8tMg38vO3E"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           ></iframe>
         </div>
-        <div className=" col-4">
+        <div className=" col">
           <iframe
-            width="425"
-            height="315"
+           height="300"
+            width="350"
             src="https://www.youtube.com/embed/SdC1gV2x8mI"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           ></iframe>
         </div>
-        <div className=" col-4">
+        <div className=" col">
           <iframe
-            width="425"
-            height="315"
+        height="300"
+            width="350"
             src="https://www.youtube.com/embed/Ap2E0RlLx24"
             title="YouTube video player"
             frameborder="0"
