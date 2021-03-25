0fbb095d5aed08438d26c4265193185243657d19
LP-Codes <lovelesh_p@yahoomail.com>
2021-03-25 19:26:58 +0530

    test

--- lp-first-react-deploy/src/Components/Issuetracker.jsx
+++ lp-first-react-deploy/src/Components/Issuetracker.jsx
@@ -1,30 +1,47 @@
-import React from "react";
-
-import "./Issuetracker.css";
+import React ,{useState} from 'react';
+import "./Issuetracker.css"
 
 const Issuetracker = () => {
+  const [input, setValue] = useState("")
+  const [todos, setTodos] = useState([])
+// passing  entered
+  const handleInput = (event) => {
+    setValue(event.target.value);
+  }
+  
+  const lp = (event) => {
+
+    let c = [1,2,34,55]
+    
+    event.preventDefault();
+  // if no input nothing will happen return none
+    if (!input) return
+    // using spread operator its used whenever we need to add array data to exiting array
+    const newTodos = [...todos, input];
+    
+    setTodos(newTodos);
+    // clearing input text
+    setValue("");
+  }
+
   return (
     <div>
-      <h2 className="text-center  alert-info font-weight-bolder">To Do List</h2>
-      <div class="input-group mb-3">
-        <input
-          type="text"
-          className="form-control font-weight-bold border border-info"
-          style={{ height: 60 }}
-          placeholder="Enter Text Here"
-        />
-        <div class="input-group-append">
-          <button
-            className="  font-weight-bolder"
-            style={{ fontSize: 20 }}
-          >
-            {" "}
-            ADD <i class="far fa-plus-square  "></i>
-          </button>
-        </div>
-      </div>
+
+<div class="input-group mb-3">
+<input className="form-control border-primary font-weight-bold" style={{height:60}} placeholder="Enter Text here" type="text" value={input} onChange={handleInput} />
+  <div class="input-group-append"> 
+  <button  className="input-group-append font-weight-bolder "  style={{fontSize:20}} onClick={lp}> <i class="fas fa-plus-square fa-2x p-2"></i> </button>
+  </div>
+</div>
+
+     
+       
+       
+     
+           
+     {todos.map((x) =><ol style={{listStyle:"none"}}><li className="font-weight-bolder">{x}</li></ol> )}
     </div>
   );
-};
+}
 
 export default Issuetracker;
